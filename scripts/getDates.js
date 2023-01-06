c44e14d2f6d66a852d472c3fbeb0b3bbb861e8fc
Jared Doerr <jared.doerr@advancedmd.com>
2023-01-05 23:55:47 -0700

    Checking W1 Assignment

---
+++ scripts/getDates.js
@@ -0,0 +1,10 @@
+function LastModified(){
+    return document.lastModified;
+}
+
+function CopyRightYear(){
+    return new Date().toLocaleDateString("en-US", {year:"numeric"});
+}
+
+document.querySelector('#copyright').innerHTML = `©${CopyRightYear()} Jared Doerr Utah, United States of America`;
+document.querySelector('#lastModified').innerHTML = `Last Modified : ${LastModified()}`;
