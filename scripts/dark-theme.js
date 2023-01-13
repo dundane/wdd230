0284882dd3684023936892a9b13ba29d928e5fcc
Jared Doerr <jared.doerr@advancedmd.com>
2023-01-12 23:29:28 -0700

    Checking in dark mode update.

---
+++ scripts/dark-theme.js
@@ -0,0 +1,18 @@
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+const darkModeIcon = "☾";
+const lightModeIcon = "☼";
+
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes(lightModeIcon)) {
+        main.classList.remove("dark-mode");
+        main.classList.add("light-mode");
+		modeButton.textContent = darkModeIcon;
+	} else {
+        main.classList.remove("light-mode");
+        main.classList.add("dark-mode");
+
+		modeButton.textContent = lightModeIcon;
+	}
+});
